30a928fa9ff37df5091e9f6dde9be08c4e51aa81
yulinfeng16 <foooyou541oo@163.com>
2023-03-18 10:00:34 +0800

    third commit

--- converter.py
+++ converter.py
@@ -1,8 +1,11 @@
 #-*-coding:UTF-8-*-
 import sys
 if __name__=="__main__":
-    cpiurl="init"
+    cpiurl="default_in.txt"
     if len(sys.argv)>1:
         cpiurl=sys.argv[1]
     print("接收到："+cpiurl)
     with open (cpiurl) as file_object:
+        for line in file_object:
+            lines=file_object.readlines()
+    print(lines)
